4a6273b4e5e605a46976780ae0f82c58e1e8b0e0
maximili.jeannere <maximili.jeannere@IG13-JeannereM.intra.eiaj.ch>
2015-10-29 08:21:40 +0100

    Initial commit

---
+++ src/java/ch/hearc/ig/odi/customeraccount/business/Customer.java
@@ -0,0 +1,52 @@
+package ch.hearc.ig.odi.customeraccount.business;
+
+import java.util.*;
+
+public class Customer {
+
+	private Map<String,Account> accounts;
+	private int number;
+	private String firstName;
+	private String lastName;
+
+	/**
+	 * 
+	 * @param number
+	 * @param firstName
+	 * @param lastName
+	 */
+	public Customer(int number, String firstName, String lastName) {
+            this.number = number;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            
+            this.accounts = new HashMap<>();
+        }
+
+	/**
+	 * 
+	 * @param number
+	 */
+	public Account getAccountByNumber(String number) {
+      
+            return accounts.get(number);
+      
+            
+            
+            
+            
+            
+	}
+
+	/**
+	 * 
+	 * @param number
+	 * @param name
+	 * @param rate
+	 */
+	public void addAccount(String number, String name, double rate) {
+		// TODO - implement Customer.addAccount
+		throw new UnsupportedOperationException();
+	}
+
+}
