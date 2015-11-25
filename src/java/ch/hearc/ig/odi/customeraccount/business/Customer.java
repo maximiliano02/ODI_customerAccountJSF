5aba0ba5f86438488370b5f9a9d8aeba11a2cc15
maximiliano02 <maxo2037@gmail.com>
2015-11-25 17:11:17 +0100

    Classe business client

--- src/java/ch/hearc/ig/odi/customeraccount/business/Customer.java
+++ src/java/ch/hearc/ig/odi/customeraccount/business/Customer.java
@@ -4,49 +4,74 @@ import java.util.*;
 
 public class Customer {
 
-	private Map<String,Account> accounts;
-	private int number;
-	private String firstName;
-	private String lastName;
+    private Map<String, Account> accounts;
+    private int number;
+    private String firstName;
+    private String lastName;
 
-	/**
-	 * 
-	 * @param number
-	 * @param firstName
-	 * @param lastName
-	 */
-	public Customer(int number, String firstName, String lastName) {
-            this.number = number;
-            this.firstName = firstName;
-            this.lastName = lastName;
-            
-            this.accounts = new HashMap<>();
-        }
+    /**
+     *
+     * @param number
+     * @param firstName
+     * @param lastName
+     */
+    public Customer(int number, String firstName, String lastName) {
+        this.number = number;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accounts = new HashMap<String, Account>();
+    }
 
-	/**
-	 * 
-	 * @param number
-	 */
-	public Account getAccountByNumber(String number) {
-      
-            return accounts.get(number);
-      
-            
-            
-            
-            
-            
-	}
+    /**
+     *
+     * @param number
+     */
+    public Account getAccountByNumber(String number) {
+        return accounts.get(number);
+    }
 
-	/**
-	 * 
-	 * @param number
-	 * @param name
-	 * @param rate
-	 */
-	public void addAccount(String number, String name, double rate) {
-		// TODO - implement Customer.addAccount
-		throw new UnsupportedOperationException();
-	}
+    /**
+     *
+     * @param number
+     * @param name
+     * @param rate
+     */
+    public void addAccount(String number, String name, double rate) {
+        
+        accounts.put(number, new Account(number, name, rate, this));       
+        
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Map<String, Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(Map<String, Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
 }
