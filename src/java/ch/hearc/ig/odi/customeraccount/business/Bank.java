669b64e292f2c72a9736bd280f9462e72d0de472
maximiliano02 <maxo2037@gmail.com>
2015-11-25 17:11:04 +0100

    Classe business banque

--- src/java/ch/hearc/ig/odi/customeraccount/business/Bank.java
+++ src/java/ch/hearc/ig/odi/customeraccount/business/Bank.java
@@ -3,30 +3,70 @@ package ch.hearc.ig.odi.customeraccount.business;
 import java.util.HashSet;
 import ch.hearc.ig.odi.customeraccount.business.Customer;
 import ch.hearc.ig.odi.customeraccount.business.Account;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Bank {
 	private int number;
 	private String name;
-	private HashSet<Customer> customers = new HashSet<Customer>();
-	private HashSet<Account> accounts = new HashSet<Account>();
+	private Map<Integer, Customer> customers;
+	private Map<String, Account> accounts;
 
 	public Bank(int number, String name) {
-		throw new UnsupportedOperationException();
+            this.number = number;
+            this.name = name;
+            
+            customers = new HashMap<Integer, Customer>();
+            accounts = new HashMap<String, Account>();
 	}
 
 	public Account getAccountByNumber(String number) {
-		throw new UnsupportedOperationException();
+            Account account = null;
+            
+            if(accounts.containsKey(number)){
+                account = accounts.get(number);
+            }
+                    
+            return account;
 	}
 
 	public Customer getCustomerByNumber(int number) {
-		throw new UnsupportedOperationException();
+            Customer customer = null;
+            
+            if(customers.containsKey(number)){
+                customer = customers.get(number);
+            }
+            
+            return customer;
 	}
 
-	public void addAccount(int number, String firstName, String lastName) {
-		throw new UnsupportedOperationException();
+        public void addCustomer(int number, String firstName, String lastName) {
+            Customer customer = new Customer(number, firstName, lastName);
+            customers.put(customer.getNumber(), customer);
 	}
-
-	public void addCustomer(String number, String name, double rate, Customer customer) {
-		throw new UnsupportedOperationException();
+        
+        public void addCustomer(Customer customer) {
+            customers.put(customer.getNumber(), customer);
 	}
+        
+	public void addAccount(String number, String name, double rate, Customer customer) {
+            customer.addAccount(number, name, rate);
+            
+            accounts.put(number, customer.getAccountByNumber(number));
+	}
+        
+        public Map<Integer, Customer> getCustomers(){
+            return customers;
+        }
+        
+        public void CreateCustomers(){
+            Customer cust = new Customer(1, "Jean", "Dupont");
+            addCustomer(cust);
+            
+            Customer cust2 = new Customer(2, "Marie", "Froideveau");
+            addCustomer(cust2);
+            
+            Customer cust3 = new Customer(3, "Alex", "Dupertuis");
+            addCustomer(cust3);
+        }
 }
