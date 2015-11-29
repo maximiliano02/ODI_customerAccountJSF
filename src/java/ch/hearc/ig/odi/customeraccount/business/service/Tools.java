a79a3646aee6baec32e30090106a3a5a6d8c1e6e
maximiliano02 <maxo2037@gmail.com>
2015-11-29 15:35:02 +0100

    Tools, recherche de bean

---
+++ src/java/ch/hearc/ig/odi/customeraccount/business/service/Tools.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.customeraccount.business.service;
+
+import javax.faces.context.FacesContext;
+
+
+public class Tools {
+    
+    public static <T> T findBean(String managedBeanName, Class<T> beanClass){
+        FacesContext context = FacesContext.getCurrentInstance();
+        return beanClass.cast(context.getApplication().evaluateExpressionGet(context, "#{" + managedBeanName +"}", beanClass));
+    }
+    
+}
