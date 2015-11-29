fcc3e8de02fe886a5a4a7d8fb451a27a7df42fe0
maximiliano02 <maxo2037@gmail.com>
2015-11-29 16:16:49 +0100

    Fonctionnalité détail d'un compte avec navigation

---
+++ src/java/ch/hearc/ig/odi/customeraccount/business/service/AccountDetailBean.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.customeraccount.business.service;
+
+import ch.hearc.ig.odi.customeraccount.business.Account;
+import ch.hearc.ig.odi.customeraccount.business.Customer;
+import java.io.Serializable;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author maximili.jeannere
+ */
+@Named(value="accountDetailBean")
+@SessionScoped
+public class AccountDetailBean implements Serializable{
+
+    @Inject Services services;
+
+    //private Customer customer;
+    private int numCustomer;
+    private String number;
+    private String name;
+    private double balance;
+    private double rate;
+
+    public String returnToCustomer(){
+        CustomerBean customerBean = Tools.findBean("customerBean", CustomerBean.class);
+        customerBean.editCustomer(numCustomer);
+        return "success";
+    }
+
+    public int getNumCustomer() {
+        return numCustomer;
+    }
+
+    public void setNumCustomer(int numCustomer) {
+        this.numCustomer = numCustomer;
+    }    
+    
+    /*public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }*/
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+}
